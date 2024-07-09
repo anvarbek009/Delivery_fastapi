4ecf938c2fb337831e143a26160b561f8a414a80
anvarbek009 <anvarasqarov2009@gmail.com>
2024-07-09 22:32:34 +0500

    commit

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from database import Base
-from sqlalchemy import Column, Integer, String,ForeignKey,Boolean,Text,Float
+from sqlalchemy import Column, Integer, String, ForeignKey, Boolean, Text, Float
 from sqlalchemy.orm import relationship
 from sqlalchemy_utils.types import ChoiceType
 
@@ -11,38 +11,37 @@ class User(Base):
     password = Column(Text)
     is_active = Column(Boolean, default=True)
     is_staff = Column(Boolean, default=False)
-    orders= relationship("Order",back_populates='user')
+    orders = relationship("Order", back_populates='user')
 
     def __repr__(self):
         return f"<User(username='{self.username}', email='{self.email}')>"
-    
+
 class Product(Base):
     __tablename__ = 'products'
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(255), unique=True, index=True)
     description = Column(Text)
     price = Column(Float)
-    orders = relationship("Order",back_populates='product')
+    orders = relationship("Order", back_populates='product')
 
     def __repr__(self):
         return f"<Product(name='{self.name}', price='{self.price}')>"
-    
 
 class Order(Base):
-    ORDER_STATUS =(
+    ORDER_STATUS = (
         ('PENDING', 'pending'),
-        ('IN_TRANSIT','in_transit'),
-        ('DELIVERED','delivered'),
-        ('CANCELLED','cancelled')
+        ('IN_TRANSIT', 'in_transit'),
+        ('DELIVERED', 'delivered'),
+        ('CANCELLED', 'cancelled')
     )
     __tablename__ = 'orders'
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey('users.id'))
-    product_id = Column(Integer, ForeignKey('product.id'))
-    status = Column(ChoiceType(ORDER_STATUS), default='pending')
+    product_id = Column(Integer, ForeignKey('products.id'))
+    status = Column(ChoiceType(ORDER_STATUS), default='PENDING')
     user = relationship("User", back_populates='orders')
     product = relationship("Product", back_populates='orders')
-    quanty=Column(Integer, nullable=False)
+    quantity = Column(Integer, nullable=False)
 
     def __repr__(self):
         return f"<Order(user_id='{self.user_id}', product_id='{self.product_id}', status='{self.status}')>"
